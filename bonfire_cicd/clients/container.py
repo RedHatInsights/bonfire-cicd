88c7530d2c2d9a1f19687aa7770f0b4b731b3011
Patrik Segedy <psegedy@redhat.com>
2022-04-26 16:39:14 +0200

    INSIGHTSQA-3270 use system podman instead of podman-py

--- bonfire_cicd/clients/container.py
+++ bonfire_cicd/clients/container.py
@@ -14,9 +14,6 @@ from docker.models.images import Image as DockerImage
 from docker.utils import parse_repository_tag
 from invoke import run
 from invoke.exceptions import UnexpectedExit
-from podman import PodmanClient
-from podman.domain.images import Image as PodmanImage
-from podman.errors import BuildError as PodmanBuildError
 
 log = logging.getLogger(__name__)
 
@@ -24,21 +21,18 @@ log = logging.getLogger(__name__)
 @attr.s
 class ContainerClient:
     base_url: Optional[str] = attr.ib(default=None)
-    auth: dict = attr.ib(init=False, default=attr.Factory(dict))
+    client: Union["PodmanClient", DockerClient] = attr.ib()
 
-    def __attrs_post_init__(self) -> None:
+    @client.default
+    def _client_factory(self):
         if self.podman_available():
-            if self.base_url:
-                self.client = PodmanClient(base_url=self.base_url)
-            else:
-                # Podman client can't handle base_url=None
-                self.client = PodmanClient()
-        else:
-            self.client = DockerClient(base_url=self.base_url)
+            return PodmanClient()
+        return DockerClient(base_url=self.base_url)
 
     @classmethod
-    def from_env(cls):
-        cls.client = PodmanClient.from_env() if cls.podman_available() else DockerClient.from_env()
+    def from_env(cls) -> "ContainerClient":
+        client = PodmanClient.from_env() if cls.podman_available() else DockerClient.from_env()
+        return cls(client=client)
 
     @staticmethod
     def podman_available() -> bool:
@@ -47,62 +41,27 @@ class ContainerClient:
         except UnexpectedExit:
             return False
 
-    def _podman_auth(self, repository) -> Optional[dict]:
-        """Workaround for missing PodmanClient.login."""
-        if isinstance(self.client, PodmanClient):
-            for registry in self.auth:
-                if registry in repository:
-                    return self.auth[registry]
-        return None
-
-    def _podman_pull_from_dockerfile(self, path, dockerfile):
-        if isinstance(self.client, PodmanClient):
-            dockerfile_path = f"{path}/{dockerfile}"
-            images = []
-            with open(dockerfile_path, "r") as f:
-                lines = f.readlines()
-                for line in lines:
-                    if line.lower().startswith("from"):
-                        images.append(line.split()[1])
-            for image in images:
-                repository, tag = parse_repository_tag(image)
-                auth_config = self._podman_auth(repository)
-                self.pull(repository, tag, auth_config=auth_config)
-
     def login(
         self,
         username: str,
         password: str,
         registry: str,
         **kwargs,
-    ) -> Dict[str, Any]:
-        if isinstance(self.client, PodmanClient):
-            # `login` method is not implemented in podman-py
-            # create auth dict which will be used for pull/push
-            self.auth.update({registry: {"username": username, "password": password}})
-            return self.auth
+    ) -> Union[Dict[str, Any], str]:
         return self.client.login(username=username, password=password, registry=registry, **kwargs)
 
     def pull(
         self, repository: str, tag: Optional[str] = None, all_tags: bool = False, **kwargs
-    ) -> Union[Union[PodmanImage, DockerImage], List[Union[PodmanImage, DockerImage]]]:
-        auth_config = kwargs.get("auth_config") or self._podman_auth(repository)
-        if auth_config:
-            return self.client.images.pull(
-                repository, tag, all_tags, auth_config=auth_config, **kwargs
-            )
-        # podman client can't handle auth_config=None in kwargs
+    ) -> Union[Union[DockerImage, List[DockerImage]], str]:
         return self.client.images.pull(repository, tag, all_tags, **kwargs)
 
     def build(
         self, path: str, tag: str, dockerfile: str, **kwargs
-    ) -> Tuple[Union[PodmanImage, DockerImage], Iterator[bytes]]:
-        self._podman_pull_from_dockerfile(path, dockerfile)
+    ) -> Union[Tuple[DockerImage, Iterator[bytes]], str]:
         if kwargs.get("cache_from"):
             log.info("Attempting to build image using cache")
             repository, __ = parse_repository_tag(tag)
-            auth_config = kwargs.get("auth_config") or self._podman_auth(repository)
-            self.pull(repository=repository, auth_config=auth_config, cache_from=True)
+            self.pull(repository=repository, cache_from=True)
             try:
                 return self.client.images.build(
                     path=path,
@@ -110,7 +69,7 @@ class ContainerClient:
                     dockerfile=dockerfile,
                     cache_from=[kwargs["cache_from"]],
                 )
-            except (PodmanBuildError, DockerBuildError):
+            except DockerBuildError:
                 log.info("Build from cache failed, attempting build without cache")
                 pass
         return self.client.images.build(
@@ -121,11 +80,75 @@ class ContainerClient:
 
     def push(
         self, repository: str, tag: Optional[str] = None, **kwargs
-    ) -> Union[str, Iterator[Union[str, Dict[str, Any]]]]:
-        auth_config = kwargs.get("auth_config") or self._podman_auth(repository)
-        if auth_config:
-            return self.client.images.push(
-                repository=repository, tag=tag, auth_config=auth_config, **kwargs
-            )
-        # podman client can't handle auth_config=None in kwargs
+    ) -> Union[Union[str, Iterator[Union[str, Dict[str, Any]]]], str]:
         return self.client.images.push(repository=repository, tag=tag, **kwargs)
+
+
+class PodmanClient:
+    """Podman client using system podman."""
+
+    def __init__(self) -> None:
+        self.images = PodmanImages()
+        self.containers = PodmanContainers()
+
+    @classmethod
+    def from_env(cls) -> "PodmanClient":
+        """Just return PodmanClient for consistency with DockerClient."""
+        return cls()
+
+    def login(
+        self,
+        username: str,
+        password: str,
+        registry: str,
+        **kwargs,
+    ) -> str:
+        """Run podman login."""
+        result = run(f"podman login -u={username!r} -p={password!r} {registry}")
+        return result.stdout
+
+
+class PodmanImages:
+    @staticmethod
+    def pull(repository: str, tag: Optional[str] = None, all_tags: bool = False, **kwargs) -> str:
+        """Run podman pull."""
+        all_tags_arg = "--all-tags" if all_tags else ""
+        tag_arg = f":{tag}" or ""
+        result = run(f"podman pull {repository}{tag_arg} {all_tags_arg}")
+        return result.stdout
+
+    @staticmethod
+    def build(path: str, tag: str, dockerfile: str, **kwargs) -> str:
+        """Run podman build."""
+        try:
+            result = run(f"podman build -f {path}/{dockerfile} -t {tag} {path}")
+        except Exception as err:
+            raise DockerBuildError(str(err), None)
+        return result.stdout
+
+    @staticmethod
+    def push(repository: str, tag: Optional[str] = None, **kwargs) -> str:
+        """Run podman push."""
+        result = run(f"podman push {repository}:{tag or 'latest'}")
+        return result.stdout
+
+
+class PodmanContainers:
+    @staticmethod
+    def run(
+        image,
+        command,
+        network_mode,
+        name,
+        entrypoint,
+        tty,
+    ):
+        tty_arg = "-t" if tty else ""
+        run(
+            f"podman run {tty_arg} --net={network_mode} --name={name}"
+            f" --entrypoint={entrypoint} {image} -c {command}"
+        )
+
+    @staticmethod
+    def cp(name, from_path, to_path):
+        run(f"podman cp {name}:{from_path} {to_path}")
